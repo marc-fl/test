34d18a1aed1fe57625cab9b35ad276714d9784ae
marc-fl <marc-fleischmann@gmx.de>
2014-01-14 18:28:18 +0100

    blaaaaaaaaaa

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -1,10 +1,34 @@
 package test;
+import java.io.*;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(new File("_filename_")));
+            String line = null;
+            while((line = br.readLine()) != null) {
+                // Ganze Zeile:
+                // System.out.println(line);                
+                String[] parts = line.split(";");
+                System.out.println("Part 1" + parts[0]);
+                System.out.println("Part 2" + parts[1]);
+                // ...
+            }
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(br != null) {
+                try {
+                    br.close();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
